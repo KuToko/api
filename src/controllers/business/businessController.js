a46368d5fee58a1be3a16e1de03aec241b198cb4
satriadinatati <satriadinata.ratnanto@ti.ukdw.ac.id>
2023-06-01 23:22:07 +0700

    Add validator in businessController

--- src/controllers/business/businessController.js
+++ src/controllers/business/businessController.js
@@ -3,6 +3,8 @@ const { Op } = require('sequelize');
 const { QueryTypes } = require('sequelize');
 const Sequelize = require('sequelize');
 
+const validator = require('fastest-validator');
+
 
 const findStore = async (req, res) => {
     try {
@@ -10,17 +12,22 @@ const findStore = async (req, res) => {
         const longitude = req.query.longitude;
         const q = req.query.q;
 
-        // const sql = `SELECT  businesses.name, categories.name as category,
-        // 6371 * ACOS(COS(RADIANS(CAST(latitude AS FLOAT))) * COS(RADIANS(CAST( ${latitude} AS FLOAT))) * COS(RADIANS(CAST(longitude AS FLOAT)) - RADIANS(CAST(${longitude} AS FLOAT))) + SIN(RADIANS(CAST(latitude AS FLOAT))) * SIN(RADIANS(CAST( ${latitude} AS FLOAT)))) AS distance_in_m,
-        // 6371 * ACOS(COS(RADIANS(CAST(latitude AS FLOAT))) * COS(RADIANS(CAST( ${latitude} AS FLOAT))) * COS(RADIANS(CAST(longitude AS FLOAT)) - RADIANS(CAST(${longitude} AS FLOAT))) + SIN(RADIANS(CAST(latitude AS FLOAT))) * SIN(RADIANS(CAST( ${latitude} AS FLOAT)))) / 1000 AS distance_in_km
-        // FROM businesses
-        // INNER JOIN business_categories ON businesses.id = business_categories.business_id
-        // INNER JOIN categories ON business_categories.category_id = categories.id
-        // WHERE businesses.name LIKE '%${q}%' OR categories.name LIKE '%${q}%'
-        // ORDER BY distance_in_m
-        // LIMIT 20`;
-        
-        // const get_business = await sequelize.query(sql);
+        const data = {latitude, longitude, q}
+
+        const schema = {
+            latitude: {type: "string", optional: false},
+            longitude: {type: "string", optional: false},
+            q: {type: "string", optional: false}
+        }
+        const v = new validator();
+        const validate = v.validate(data, schema);
+
+        if (validate.length) {
+            return res.status(400).json({
+                message: "error",
+                data: validate
+            });
+        }
 
         const name = `%${q}%`;
 
@@ -35,8 +42,8 @@ const findStore = async (req, res) => {
         ORDER BY distance_in_m
         LIMIT 20
         `, {
-        replacements: { latitude, longitude, name },
-        type: Sequelize.QueryTypes.SELECT,
+            replacements: { latitude, longitude, name },
+            type: Sequelize.QueryTypes.SELECT,
         });
           
         
