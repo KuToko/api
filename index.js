66070425723a99d85d15a042ea56a2a31f6e645d
satriadinatati <satriadinata.ratnanto@ti.ukdw.ac.id>
2023-06-07 01:39:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(fileUpload({
 app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => {
-    res.send('Hello World ehe');
+    res.send('<center style="font-size:40px;margin-top:23%">Kutoko Service 👋</center>');
 })
 app.use("/v1", routesV1);
 
